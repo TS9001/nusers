c3a0457ac5cba3778d539d6201370c3e99186f74
Tomas Sikora <ttomassikora@gmail.com.com>
2018-12-27 00:39:28 +0100

    Basic project structure and dependencies

---
+++ src/main/java/com/usern/NPhotos.java
@@ -0,0 +1,15 @@
+package com.usern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class NPhotos {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NPhotos.class, args);
+    }
+}
+
